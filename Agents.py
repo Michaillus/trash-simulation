b49c04f777b9a05f295dcd6ccb258980b955543b
Michaillus <mihail.ab3@gmail.com>
2025-03-30 03:30:22 +0200

    Made a basic skeleton on of the simulation

---
+++ Agents.py
@@ -0,0 +1,16 @@
+from mesa.experimental.continuous_space.continuous_space_agents import ContinuousSpaceAgent
+
+class Robot(ContinuousSpaceAgent):
+    def __init__(self, space, model):
+        pass
+
+    def charge(self):
+        """I'm charging"""
+
+class Human(ContinuousSpaceAgent):
+    def __init__(self, space, model):
+        pass
+
+class Trash(ContinuousSpaceAgent):
+    def __init__(self, space, model):
+        pass
---
+++ Model.py
@@ -0,0 +1,8 @@
+from mesa import Model
+
+class TrashCollection(Model):
+    def __init__(self):
+        pass
+
+    def step(self):
+        pass
