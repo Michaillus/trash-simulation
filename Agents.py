2c21fad61bef182ecdc2121c16dc4093f647a970
Michaillus <mihail.ab3@gmail.com>
2025-03-30 20:11:36 +0200

    Added basic setup of the model and agents

--- Agents.py
+++ Agents.py
@@ -1,16 +1,38 @@
+import random
+
 from mesa.experimental.continuous_space.continuous_space_agents import ContinuousSpaceAgent
 
 class Robot(ContinuousSpaceAgent):
-    def __init__(self, space, model):
-        pass
+    def __init__(self, model, space, initial_x = 0, initial_y = 0, max_energy = 100, max_speed = 10):
+        super().__init__(space, model)
+
+        # Set position of the robot
+        self.position[0] = initial_x
+        self.position[1] = initial_y
+
+    def step(self):
+        print(f"I am a robot {self.unique_id} at {self.position[0]}, {self.position[1]}")
+
+    def move(self, speed):
+        print("Robot is moving")
+
+    def sweep(self):
+        print("Robot is sweeping")
 
     def charge(self):
-        """I'm charging"""
+        print("Robot is charging")
 
 class Human(ContinuousSpaceAgent):
-    def __init__(self, space, model):
-        pass
+    def __init__(self, model, space):
+        super().__init__(space, model)
+
+        # Set initial position of the human
+        self.position[0] = random.uniform(0, self.space.width)
+        self.position[1] = random.uniform(0, self.space.height)
+
+    def step(self):
+        print(f"I am a human {self.unique_id} at {self.position[0]}, {self.position[1]}")
 
 class Trash(ContinuousSpaceAgent):
     def __init__(self, space, model):
-        pass
+        super().__init__(space, model)
--- Model.py
+++ Model.py
@@ -1,8 +1,54 @@
+import random
+
 from mesa import Model
+from mesa.experimental.continuous_space.continuous_space import ContinuousSpace
+from Agents import Human
+from Agents import Robot
 
 class TrashCollection(Model):
-    def __init__(self):
-        pass
+    def __init__(
+            self,
+            width = 100,
+            height = 20,
+            nr_of_people = 4,
+            trash_spawn_rate = 0,
+            people_speed = 0,
+            robot_speed = 0,
+            seed = None
+        ):
+
+        super().__init__(seed=seed)
+
+        self.width = width
+        self.height = height
+
+        # Create a continuous space
+        dimensions = [[0, width], [0, height]]
+        rng = random.Random(42)
+        self.space = ContinuousSpace(dimensions, torus=False, random=rng)
+
+        # Create robot
+        Robot.create_agents(
+            self,
+            1,
+            space=self.space,
+            initial_x=10,
+            initial_y=10
+        )
+
+        # Create people
+        Human.create_agents(
+            self,
+            nr_of_people,
+            space=self.space
+        )
+
 
     def step(self):
-        pass
+        # First activate all the people
+        self.agents_by_type[Human].shuffle_do("step")
+        # Then activate the robot
+        self.agents_by_type[Robot].do("step")
+
+trash_collection = TrashCollection()
+trash_collection.step()
